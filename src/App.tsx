9f59242551a27432ff1ae5eed8f6668a7c7676ea
edutel <15623704+EduTel@users.noreply.github.com>
2024-04-23 18:28:15 -0600

    feat: onSnapshot

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,27 @@ import { useEffect, useState } from "react";
 import { getLisitaCollection, db, lisitaDocumentCollection } from "./firebase";
 import PrintCollection from "./components/PrintCollection";
 import { MyForm } from "./components/MyForm";
+import { onSnapshot, collection } from "@firebase/firestore";
 
 function App() {
   const [documentsLisita, setDocumentsLisita] =
     useState<lisitaDocumentCollection[]>();
+
   useEffect(() => {
-    getLisitaCollection(db).then((data) => setDocumentsLisita(data));
+    const unsubscribe = onSnapshot(collection(db, "lisita"), (snapshot) => {
+      setDocumentsLisita(
+        snapshot.docs.map((data) => data.data() as lisitaDocumentCollection)
+      );
+    });
+
+    return () => {
+      unsubscribe();
+    };
   }, []);
   return (
     <>
       <MyForm />
-      {documentsLisita?.map((document, index) => (
+      {documentsLisita?.map?.((document, index) => (
         <PrintCollection key={index} collection={document} />
       ))}
     </>
