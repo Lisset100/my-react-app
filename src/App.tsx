ea8dda37e1314fc6cd4fc39d82cc6a538e3d48de
edutel <15623704+EduTel@users.noreply.github.com>
2024-04-27 10:28:32 -0600

    fix: remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getLisitaCollection, db, lisitaDocumentCollection } from "./firebase";
+import { db, lisitaDocumentCollection } from "./firebase";
 import PrintCollection from "./components/PrintCollection";
 import { MyForm } from "./components/MyForm";
 import { onSnapshot, collection } from "@firebase/firestore";
