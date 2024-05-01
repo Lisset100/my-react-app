a5310e606d37a5fbe8120304817e6a1441b1d91d
edutel <15623704+EduTel@users.noreply.github.com>
2024-05-01 00:20:31 -0600

    feat: mui table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { useEffect, useState } from "react";
-import { db, lisitaDocumentCollection } from "./firebase";
-import PrintCollection from "./components/PrintCollection";
+import { db, handleDeleteOnlyOne, lisitaDocumentCollection } from "./firebase";
 import { MyForm } from "./components/MyForm";
 import { onSnapshot, collection } from "@firebase/firestore";
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 function App() {
   const [documentsLisita, setDocumentsLisita] =
@@ -24,9 +33,41 @@ function App() {
   return (
     <div>
       <MyForm />
-      {documentsLisita?.map?.((document, index) => (
-        <PrintCollection key={index} collection={document} />
-      ))}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {["Name", "Age", "Latitude", "Longitude", "Delete"].map(
+                (data: string) => {
+                  return <TableCell align="right">{data}</TableCell>;
+                }
+              )}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {documentsLisita?.map((row, id) => (
+              <TableRow
+                key={"row" + id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.age}</TableCell>
+                <TableCell align="right">{row.location.latitude}</TableCell>
+                <TableCell align="right">{row.location.longitude}</TableCell>
+                <TableCell align="right">
+                  <Button
+                    variant="contained"
+                    onClick={() => handleDeleteOnlyOne(row.id)}
+                    color="error"
+                  >
+                    Eliminar Datos
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
