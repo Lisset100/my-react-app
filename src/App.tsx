7665cd060a5a36b2625f9cbcd7099adfc11a6eb7
edutel <15623704+EduTel@users.noreply.github.com>
2024-05-02 09:34:17 -0600

    fix: style order

--- src/App.tsx
+++ src/App.tsx
@@ -62,68 +62,81 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <MyForm />
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              {["Name", "Age", "Latitude", "Longitude", "Delete"].map(
-                (data: string) => {
-                  return <TableCell align="right">{data}</TableCell>;
-                }
-              )}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {documentsLisita?.map((row, id) => (
-              <TableRow
-                key={"row" + id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell align="right">{row.name}</TableCell>
-                <TableCell align="right">{row.age}</TableCell>
-                <TableCell align="right">{row.location.latitude}</TableCell>
-                <TableCell align="right">{row.location.longitude}</TableCell>
-                <TableCell align="right">
-                  <Button
-                    variant="contained"
-                    onClick={() => handleDeleteOnlyOne(row.id)}
-                    color="error"
-                  >
-                    Eliminar Datos
-                  </Button>
-                </TableCell>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      <div>
+        <MyForm />
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                {["Name", "Age", "Latitude", "Longitude", "Delete"].map(
+                  (data: string) => {
+                    return <TableCell align="right">{data}</TableCell>;
+                  }
+                )}
               </TableRow>
+            </TableHead>
+            <TableBody>
+              {documentsLisita?.map((row, id) => (
+                <TableRow
+                  key={"row" + id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="right">{row.name}</TableCell>
+                  <TableCell align="right">{row.age}</TableCell>
+                  <TableCell align="right">{row.location.latitude}</TableCell>
+                  <TableCell align="right">{row.location.longitude}</TableCell>
+                  <TableCell align="right">
+                    <Button
+                      variant="contained"
+                      onClick={() => handleDeleteOnlyOne(row.id)}
+                      color="error"
+                    >
+                      Eliminar Datos
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      <div
+        style={{
+          width: "100%",
+        }}
+      >
+        {isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={{
+              margin: "auto",
+              width: "100%",
+              height: "100%",
+            }}
+            center={center}
+            zoom={12}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            {/* Child components, such as markers, info windows, etc. */}
+            {documentsLisita?.map((data) => (
+              <Marker
+                position={{
+                  lat: data.location.latitude,
+                  lng: data.location.longitude,
+                }}
+              />
             ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-
-      {isLoaded ? (
-        <GoogleMap
-          mapContainerStyle={{
-            width: "400px",
-            height: "400px",
-          }}
-          center={center}
-          zoom={12}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
-        >
-          {/* Child components, such as markers, info windows, etc. */}
-          {documentsLisita?.map((data) => (
-            <Marker
-              position={{
-                lat: data.location.latitude,
-                lng: data.location.longitude,
-              }}
-            />
-          ))}
-        </GoogleMap>
-      ) : (
-        <></>
-      )}
+          </GoogleMap>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
