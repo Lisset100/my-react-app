3795bdbe81e25dc3b63079478d099c7687b961b7
edutel <15623704+EduTel@users.noreply.github.com>
2024-05-05 15:13:38 -0600

    fix: form grid

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -4,7 +4,7 @@ import {
   lisitaDocumentCollection,
   deleteDocument,
 } from "../firebase";
-import { Button, TextField } from "@mui/material";
+import { Button, Grid, TextField } from "@mui/material";
 import { FormControl } from "@mui/base/FormControl";
 import { GeoPoint } from "firebase/firestore";
 import { StandaloneSearchBox } from "@react-google-maps/api";
@@ -54,57 +54,65 @@ export const MyForm: React.FC<MyFormProps> = (props) => {
     <div>
       <form onSubmit={handleSubmit}>
         <FormControl>
-          <div>
-            <TextField
-              id="name"
-              type="text"
-              label={"Name"}
-              value={data.name}
-              onChange={(e) =>
-                setData((beforeData) => {
-                  return { ...beforeData, name: e.target.value };
-                })
-              }
-            />
-            <TextField
-              id="age"
-              type="number"
-              label={"Age"}
-              value={data.age}
-              onChange={(e) =>
-                setData((beforeData) => ({
-                  ...beforeData,
-                  age: parseInt(e.target.value),
-                }))
-              }
-            />
-            {props.isLoaded && (
-              <StandaloneSearchBox
-                onPlacesChanged={() => {
-                  searchBox?.getPlaces()?.forEach((data) => {
-                    const location = data.geometry?.location?.toJSON();
-                    if (location) {
-                      setData((data) => ({
-                        ...data,
-                        location: new GeoPoint(location.lat, location.lng),
-                      }));
-                    }
-                  });
-                }}
-                onLoad={(ref) => {
-                  setSearchBox(ref);
-                }}
-              >
-                <TextField
-                  fullWidth
-                  label="Location"
-                  variant="outlined"
-                  placeholder=""
-                  inputRef={searchBoxRef}
-                />
-              </StandaloneSearchBox>
-            )}
-          </div>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <TextField
+                fullWidth
+                id="name"
+                type="text"
+                label={"Name"}
+                value={data.name}
+                onChange={(e) =>
+                  setData((beforeData) => {
+                    return { ...beforeData, name: e.target.value };
+                  })
+                }
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                fullWidth
+                id="age"
+                type="number"
+                label={"Age"}
+                value={data.age}
+                onChange={(e) =>
+                  setData((beforeData) => ({
+                    ...beforeData,
+                    age: parseInt(e.target.value),
+                  }))
+                }
+              />
+            </Grid>
+            <Grid item xs={12}>
+              {props.isLoaded && (
+                <StandaloneSearchBox
+                  onPlacesChanged={() => {
+                    searchBox?.getPlaces()?.forEach((data) => {
+                      const location = data.geometry?.location?.toJSON();
+                      if (location) {
+                        setData((data) => ({
+                          ...data,
+                          location: new GeoPoint(location.lat, location.lng),
+                        }));
+                      }
+                    });
+                  }}
+                  onLoad={(ref) => {
+                    setSearchBox(ref);
+                  }}
+                >
+                  <TextField
+                    fullWidth
+                    label="Location"
+                    variant="outlined"
+                    placeholder=""
+                    inputRef={searchBoxRef}
+                  />
+                </StandaloneSearchBox>
+              )}{" "}
+            </Grid>
+          </Grid>
           <Button type="submit" variant="contained" color="success">
             Save Data
           </Button>
